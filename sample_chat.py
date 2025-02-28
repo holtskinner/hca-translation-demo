4cd71899bfe5584feef2524095be8defb0550f81
Holt Skinner <holtskinner@google.com>
2025-02-28 11:45:09 -0600

    Added Gemini spanish translation

--- sample_chat.py
+++ sample_chat.py
@@ -1,8 +1,26 @@
+import os
+from google import genai
+from google.genai.types import GenerateContentConfig, Part
 import streamlit as st
+
 
 # Initialize session state for chat history
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
+
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
+LOCATION = os.environ.get("GOOGLE_CLOUD_REGION")
+
+if PROJECT_ID and not LOCATION:
+    LOCATION = "us-central1"
+
+MODEL_ID = "gemini-2.0-flash-lite"
+
+
+@st.cache_resource
+def load_client() -> genai.Client:
+    """Load Google Gen AI Client."""
+    return genai.Client(vertexai=True, project=PROJECT_ID, location=LOCATION)
 
 
 # Function to display the chat history
@@ -15,6 +33,8 @@ def display_chat_history():
             with st.chat_message("assistant"):
                 st.markdown(message["text"])
 
+
+client = load_client()
 
 # Main Streamlit app
 st.title("Simple Chat App")
@@ -34,10 +54,13 @@ if user_input:
         st.markdown(user_input)
 
     # Simulate an assistant response (replace with your actual logic)
-    assistant_response = f"You said: {user_input}  (This is a placeholder response.)"  # Replace this with your AI model response
-    # Add artificial delay
-    # import time
-    # time.sleep(1)
+    assistant_response = client.models.generate_content(
+        model=MODEL_ID,
+        contents=user_input,
+        config=GenerateContentConfig(
+            system_instruction="You are an empathetic nurse who is answering a patient's questions about their upcoming surgery. Translate the following text into Spanish.",
+        ),
+    ).text
 
     # Add assistant message to chat history
     st.session_state["chat_history"].append(
